414706c9f2adf771e0c3b15e05a055e0c3526a7f
QC-Elite-648 <qcelite@qcelite.com>
2026-02-05 20:25:33 -0600

    initial climber

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -67,4 +67,12 @@ public final class Constants
     public static final int DRIVER_PORT         = 0;
     public static final int OPERATOR_PORT       = 1;
   }
+  public static final class ClimberConstants
+  {
+    public static final int kClimber1CANId = 25;
+    public static final int kClimber2CANId = 26;
+    public static final double currentMax = 1;
+    public static final int kClimber1PDH = 14;
+    public static final int kClimber2PDH = 15;
+  }
 }
